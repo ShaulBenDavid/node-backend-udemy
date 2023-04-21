32fd8057bb09d5bf5561f2cd5d9ddefffc5c0fe5
Shaul Ben david <Shaulbd9@gmail.com>
2023-04-21 09:58:00 +0200

    testing mongodb

--- test/authController.js
+++ test/authController.js
@@ -1,10 +1,29 @@
 const expect = require("chai").expect;
 const { describe } = require("mocha");
 const sinon = require("sinon");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 describe("Auth controller", () => {
+  before(function (done) {
+    mongoose
+      .connect(process.env.MONGO_DB_TEST_KEY)
+      .then(() => {
+        const user = new User({
+          email: "test@gmail.com",
+          password: "tester",
+          name: "Test",
+          _id: "507f1f77bcf86cd799439011",
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
   it("Should throw error if accessing the database fails", function (done) {
     sinon.stub(User, "findOne");
     User.findOne.throws();
@@ -24,5 +43,33 @@ describe("Auth controller", () => {
         done(err);
       });
     User.findOne.restore();
+  });
+
+  it("should send a response with a valid user status for an existing user", function (done) {
+    const req = { userId: "507f1f77bcf86cd799439011" };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status;
+      },
+    };
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.userStatus).to.be.equal("I am new");
+      done();
+    });
+  });
+
+  after(function (done) {
+    User.deleteMany({}).then(() => {
+      mongoose.disconnect().then(() => {
+        done();
+      });
+    });
   });
 });
